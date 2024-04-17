fe6f8b07d106e39e67741beaff014090b96603e4
Yvan Oliveira <85844516+yvanoliveira@users.noreply.github.com>
2024-04-17 16:08:11 -0300

    Grade student update

--- todowise/app/views.py
+++ todowise/app/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.views.decorators.csrf import csrf_protect
 
-
 from .models import User, Course, Subject, Student, Grade, Attendance, Register
 
 
@@ -62,6 +61,24 @@ def add_subject(request, course_id):
     return redirect('course_list', course_id = course_id)
 
 
+def add_grade(request, student_id, subject_id):
+    if request.method == 'POST':
+        grade_value = request.POST.get('grade')
+        student = get_object_or_404(Student, pk=student_id)
+        subject = get_object_or_404(Subject, pk=subject_id)
+
+        # Procurar por uma instância de Grade existente ou criar uma nova
+        grade, created = Grade.objects.get_or_create(student=student, subject=subject, defaults={'grade': grade_value})
+
+        if not created:
+            # Atualizar a nota se já existir
+            grade.grade = grade_value
+            grade.save()
+
+        # Retornar a nota atualizada em JSON
+        return JsonResponse({'grade': grade.grade})
+
+
 def remove_subject(request, course_id, subject_id):
     course = get_object_or_404(Course, pk = course_id, user = request.user)
     subject = get_object_or_404(Subject, pk = subject_id, course = course)
@@ -80,33 +97,26 @@ def done_subject(request, course_id, subject_id):
     subject.save()
     return redirect('course_list', course_id = course_id)
 
-
-def edit_student_drop(request, student_id, course_id):
-    student = get_object_or_404(Student, pk=student_id)
+def update_grade(request, grade_id):
     if request.method == 'POST':
-        # Receba os dados do formulário e atualize o objeto do aluno
-        student.name = request.POST.get('first_name_student')
-        student.last_name = request.POST.get('last_name_student')
-        student.birth_date = request.POST.get('birth_date')
-        student.adress = request.POST.get('adress')
-        student.email = request.POST.get('email')
-        student.celphone = request.POST.get('celphone')
-        student.course = get_object_or_404(Course, pk=course_id, user=request.user)
-        student.save()
+        new_grade = float(request.POST.get('grade'))
+        grade = Grade.objects.get(pk=grade_id)
+        grade.grade = new_grade
+        grade.save()
+    #return redirect('open_subject')  # Redireciona para a página com a lista de notas
+    return render(request, 'app/subjects.html')
 
-        #return redirect('edit_student', student_id=student_id)
-    return render(request, 'app/student.html', {'student': student})
+def update_status(request, student_id):
+    student = Student.objects.get(pk=student_id)
+    grades = Grade.objects.filter(student=student)
 
-
-def add_remove_student_class(request, student_id):
-    student = get_object_or_404(Student, pk=student_id)
-    if request.method == 'POST':
-        # Lógica para adicionar/remover o aluno de uma turma
-        # Aqui você precisa decidir como ser á essa lógica com base na estrutura do seu sistema
-        # Por exemplo, você pode ter um campo na classe Student que representa as turmas em que o aluno está matriculado
-        # E então adicionar ou remover a turma conforme necessário
-        pass
-    return render(request, 'app/add_remove_student_class.html', {'student': student})
+    for grade in grades:
+        if all(grade.grade >= 5):
+            status = "Aprovado"
+        else:
+            status = "Reprovado"
+    #status = "Aprovado" if all(grade.grade >= 5 for grade in grades) else "Reprovado"
+    return render(request, 'subjects.html', context= {'status': status})
 
 
 def edit_attendance(request, attendance_id):
@@ -119,23 +129,6 @@ def edit_attendance(request, attendance_id):
     return render(request, 'app/edit_attendance.html', {'attendance': attendance})
 
 
-def create_edit_class(request, class_id=None):
-    if class_id:
-        # Lógica para editar uma turma existente
-        pass
-    else:
-        # Lógica para criar uma nova turma
-        pass
-    return render(request, 'app/create_edit_class.html')
-
-
-def add_edit_grade(request, student_id):
-    student = get_object_or_404(Student, pk=student_id)
-    if request.method == 'POST':
-        # Receba os dados do formulário e crie/edit a nota do aluno
-        pass
-    return render(request, 'app/add_edit_grade.html', {'student': student})
-
 def open_subject(request, course_id, subject_id):
     course = get_object_or_404(Course, pk = course_id, user = request.user)
     subject = get_object_or_404(Subject, pk = subject_id, course = course)
@@ -144,7 +137,7 @@ def open_subject(request, course_id, subject_id):
     for grade in grades:
         student_info = {
             'student': grade.student,
-            'grade': grade.grade
+            'grade': grade.grade,
         }
         students_grades.append(student_info)
     return render(request, template_name = 'app/subjects.html', context = {'subject':subject, 'grades':grades})
